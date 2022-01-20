a5bb55579bd6c59d9e8f69333b039f6b33a6f746
Higor <hhigorsilvachaves@gmail.com>
2022-01-20 19:28:43 -0300

    Add actions to create a post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true
+  validates :description, presence: true, length: { in: 6..50 }
+  validates :body, presence: true, length: { in: 1..240 }
+
 end
